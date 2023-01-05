07316c7d5eec4aba8c7b90d89cb53ebada2b6d05
Vishal Patel <vpatel85@gmail.com>
2023-01-05 11:49:16 -0500

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dependencies = [
 
 setup(
     name='tiny-cli',
-    version='0.9',
+    version='0.10',
     description='TinyBio genome analysis tool',
     long_description_content_type='text/markdown',
     url='https://github.com/tinybio-cloud/tiny',
