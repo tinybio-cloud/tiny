c1719c64d8a703e23556a924e814f6a1b7972a64
Vishal Patel <vpatel85@gmail.com>
2023-03-20 14:28:49 -0400

    revert prod url

--- tiny/settings.py
+++ tiny/settings.py
@@ -1,3 +1,3 @@
-# PROD_BASE_URL = "https://api.tinybio.cloud"
-PROD_BASE_URL = "http://localhost:8080"
+PROD_BASE_URL = "https://api.tinybio.cloud"
+# PROD_BASE_URL = "http://localhost:8080"
 DEV_BASE_URL = "http://localhost:8080"
