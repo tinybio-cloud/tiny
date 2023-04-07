626b9d75c8ab9528abe3ee8e6aebe9c7d22a3e77
Vishal Patel <vpatel85@gmail.com>
2023-04-07 18:28:27 -0400

    chore(settings):revert prod url

--- tiny/settings.py
+++ tiny/settings.py
@@ -1,4 +1,4 @@
-# PROD_BASE_URL = "https://tiny-api-ppi7g2c4fq-uc.a.run.app"
+PROD_BASE_URL = "https://tiny-api-ppi7g2c4fq-uc.a.run.app"
 # PROD_BASE_URL = "https://api.tinybio.cloud"
-PROD_BASE_URL = "http://localhost:8080"
+# PROD_BASE_URL = "http://localhost:8080"
 DEV_BASE_URL = "http://localhost:8080"
